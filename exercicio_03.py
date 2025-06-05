9906bd83f7a7e0d0639b0d50ee2f9c8675eeb98c
kauan <132300475+kauanbz@users.noreply.github.com>
2025-06-05 08:30:47 -0300

    Update exercicio_03.py

--- exercicio_03.py
+++ exercicio_03.py
@@ -1,16 +1,17 @@
 def maximo_personalizado(valores: list[float]) -> float:
 
-    maior = valores[0]
-    for valor in valores[1:]:
-        if valor > maior:
-            maior = valor
-    return maior
+    mv = valores[0]
+    for v in valores[1:]:
+        if v > mv:
+            mv = v
+    return mv
 from unittest.mock import patch
 import pytest
 import importlib
+
+
 @patch("builtins.print")
 @patch("builtins.input")
-@patch("builtins.max")
 @pytest.mark.parametrize(
     argnames=["argumento", "real"],
     argvalues=[
@@ -22,21 +23,27 @@ import importlib
         ([1.1, 2.2, 3.3, 2.2, 3.3, 3.3], 3.3),  # máximo repetido várias vezes
     ],
 )
-def test_exercicio_03(max, input, print, argumento: list[float], real: float):
+def test_exercicio_03(input, print, argumento: list[float], real: float):
     exercicio_03 = importlib.reload(importlib.import_module("exercicio_03"))
-    # Verifica existência da função
-    assert hasattr(exercicio_03, "maximo_personalizado"), (
-        "Não definiu a função maximo_personalizado"
-    )
-    funcao = exercicio_03.maximo_personalizado
-    nome_funcao = funcao.__name__
-    # Executa o código do aluno
-    aluno = funcao(argumento)
-    # Garante que não usou max(), input() ou print()
-    assert not max.called, "A função max não deveria ser usada"
-    assert not input.called, "A função input não deveria ser usada"
-    assert not print.called, "A função print não deveria ser usada"
-    # Verifica resultado
-    assert aluno == real, (
-        f"A função {nome_funcao}({argumento}) deveria retornar => {real}, mas retornou {aluno}"
-    )
+
+    with patch("builtins.max") as max_func:
+
+        # Verifica existência da função
+        assert hasattr(exercicio_03, "maximo_personalizado"), (
+            "Não definiu a função maximo_personalizado"
+        )
+        funcao = exercicio_03.maximo_personalizado
+        nome_funcao = funcao.__name__
+
+        # Executa o código do aluno
+        aluno = funcao(argumento)
+
+        # Garante que não usou max(), input() ou print()
+        assert not max_func.called, "A função max não deveria ser usada"
+        assert not input.called, "A função input não deveria ser usada"
+        assert not print.called, "A função print não deveria ser usada"
+
+        # Verifica resultado
+        assert aluno == real, (
+            f"A função {nome_funcao}({argumento}) deveria retornar => {real}, mas retornou {aluno}"
+        )
