9f34cd1a338ed7e0a8846b723f25f4796d1d8ec4
kauan <132300475+kauanbz@users.noreply.github.com>
2025-06-12 14:38:09 -0300

    Update exercicio_07.py

--- exercicio_07.py
+++ exercicio_07.py
@@ -1,1 +1,10 @@
-
+def classifica_notas(notas: list[float]) -> list[str]:
+    classificacao = []
+    for n in notas:
+        if n >= 6.0:
+            classificacao.append("Aprovado")
+        elif n >= 4.0:
+            classificacao.append("Recuperação")
+        else:
+            classificacao.append("Reprovado")
+    return classificacao
