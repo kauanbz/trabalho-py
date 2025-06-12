1f90e2d0327cf2023fd6cb716bef67420202afbf
kauan <132300475+kauanbz@users.noreply.github.com>
2025-06-12 14:38:52 -0300

    Update exercicio_08.py

--- exercicio_08.py
+++ exercicio_08.py
@@ -1,1 +1,11 @@
+import pandas as pd
 
+def cria_resumo(df: pd.DataFrame, col: str) -> pd.DataFrame:
+    if col not in df.columns:
+        raise ValueError(f"Coluna '{col}' não encontrada.")
+
+    if not pd.api.types.is_numeric_dtype(df[col]):
+        raise TypeError(f"Coluna '{col}' não é numérica.")
+
+    res = df[col].describe().to_frame()
+    return res
