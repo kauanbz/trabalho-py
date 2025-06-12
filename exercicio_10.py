51329309d36bb5265b74556a26eff229dc088c6f
kauan <132300475+kauanbz@users.noreply.github.com>
2025-06-12 14:39:54 -0300

    Update exercicio_10.py

--- exercicio_10.py
+++ exercicio_10.py
@@ -1,1 +1,11 @@
+import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt
+from matplotlib.figure import Figure
 
+def plot_histograma(vals: list[float], bins: int = 10) -> Figure:
+    fig, ax = plt.subplots()
+    ax.hist(vals, bins=bins, edgecolor='black')
+    ax.set_title('Histograma')
+    ax.set_xlabel('Valores')
+    ax.set_ylabel('Frequência')
+    return fig
