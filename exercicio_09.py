0b1af9c4dc6e0115c097087c0452156b14183d49
kauan <132300475+kauanbz@users.noreply.github.com>
2025-06-12 14:39:24 -0300

    Update exercicio_09.py

--- exercicio_09.py
+++ exercicio_09.py
@@ -1,1 +1,10 @@
+import pandas as pd
 
+def filtra_df(df: pd.DataFrame, col: str, lim):
+    if col not in df.columns:
+        raise ValueError(f"Coluna '{col}' não encontrada.")
+
+    if not pd.api.types.is_numeric_dtype(df[col]):
+        raise TypeError(f"Coluna '{col}' não é numérica.")
+
+    return df[df[col] >= lim]
